97647c2ed459890caa3f37c6a6d75b62a6742679
Mitchifs <mitchifs@gmail.com>
2020-11-19 17:38:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -954,14 +954,17 @@ bot.on("message", async message => {
 			let calculsRestants = nombreDeCalculs
 			while(calculsRestants > 0){
 				const quantitéNombres = Math.floor(Math.random()*(nbOpérationsMax-nbOpérationsMin))+nbOpérationsMax
+				console.log(quantitéNombres)
 				let nombres = []
 				let opérations = []
 				for(let i = 0 ; i < quantitéNombres ; i++){
 					nombres.push(Math.floor(Math.random()*puissanceDe10Max*10))
 				}
+				console.log(nombres)
 				for(let i = 0 ; i < quantitéNombres-1 ; i++){
 					opérations.push(opérationsPossibles[Math.floor(Math.random()*opérationsPossibles.length)])
 				}
+				console.log(opérations)
 				let messageCalcul = ""
 				let résultat = 0
 				for(let i = 0 ; i < quantitéNombres; i++){
