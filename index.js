3d0d0912e98e0fda093e818cd964e13d77f95a51
Mitchifs <mitchifs@gmail.com>
2020-11-19 18:06:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -970,6 +970,7 @@ bot.on("message", async message => {
 				for(let i = 0 ; i < quantitéNombres; i++){
 					messageCalcul += `${nombres[i]}`
 					if(i === quantitéNombres-1){
+						console.log(messageCalcul)
 						résultat = eval(messageCalcul)
 						messageCalcul += ` = ?\` (${temps}s)`
 					}
