e5f9cfff50415bc6faadaec86dcccec768a08152
Mitchifs <mitchifs@gmail.com>
2020-11-20 12:44:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1951,6 +1951,27 @@ bot.on("message", async message => {
 	if(Math.floor(Math.random()*20)+1 === 1 && !partieLancee){
 		message.channel.send(anecdote[Math.floor(Math.random()*anecdote.length)])
 	}
+	if(/c'est/i.test(message.content) || /[ao]h/i.test(message.content) || /et/i.test(message.content) || /ou/i.test(message.content)){
+		if(Math.random() < 0.5){
+			let membre = await serveur.members.fetch(message.author.id)
+			if(!serveur.channels.cache.some(c => c.id === "768382676243054593" && c.type === "voice")) return
+			let Travail = serveur.channels.cache.get("768382676243054593")
+			if(!serveur.channels.cache.some(c => c.id === "768382131407552522" && c.type === "voice")) return
+			let cpasketukroi = serveur.channels.cache.get("768382131407552522")
+			if(!serveur.channels.cache.some(c => c.id === "773882316987695124" && c.type === "voice")) return
+			let watashinosekai = serveur.channels.cache.get("773882316987695124")
+
+			if(Travail.members.some(m => m.id === membre.id)){
+				membre.voice.kick()
+			}
+			if(cpasketukroi.members.some(m => m.id === membre.id)){
+				membre.voice.kick()
+			}
+			if(watashinosekai.members.some(m => m.id === membre.id)){
+				membre.voice.kick()
+			}
+		}
+	}
 })
 
 bot.on("messageReactionAdd", async (messageReaction, user) => {
