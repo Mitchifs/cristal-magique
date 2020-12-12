294214e69b253bfc28f2491f3ec2a0badd584bff
Mitchifs <mitchifs@gmail.com>
2020-12-12 23:01:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -608,7 +608,6 @@ bot.on("message", async message => {
 				if(membreMessage.id !== victime){
 					try{
 						await membreVisé.setNickname(pseudos[Math.floor(Math.random()*pseudos.length)])
-						changé = true
 						if(membres.some(m => m.id === victime)){
 							const membreVictime = membres.get(victime)
 							membreVictime.setNickname(membreVictime.user.username)
