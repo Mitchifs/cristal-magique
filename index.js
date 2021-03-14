201dc43a9436509488f86b8756c0176f1ceb675f
Mitchifs <mitchifs@gmail.com>
2021-03-14 23:49:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2138,7 +2138,7 @@ bot.on("typingStart", async (channel,user) => {
 				"xptdrlol",
 			]
 			const mot = mots[Math.floor(Math.random()*mots.length)]
-			user.send(`Utilise "${mot}" ou sinon tu seras banni.`)
+			user.send(`Utilise "${mot}" dans ton prochain message ou sinon tu seras banni.`)
 		}
 	}
 })
